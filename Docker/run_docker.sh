0bbf752faa0abd41557834ecf3ec8012498d324a
Brian Haas <bhaas@broadinstitute.org>
2023-06-20 23:06:19 +0000

    added k8 to docker

--- Docker/run_docker.sh
+++ Docker/run_docker.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker run --rm -it -v `pwd`:/data -v ${CTAT_GENOME_LIB}:/ctat_genome_lib  trinityctat/gmapfusion:latest $*
+docker run --rm -it -v `pwd`:/data -v ${CTAT_GENOME_LIB}:/ctat_genome_lib  -w /data trinityctat/minimap2fusion:latest $*
 
