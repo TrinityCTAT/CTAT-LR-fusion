8ef8c1c0de6082c219a567c5b187080f59b0dee6
Brian Haas <brianjohnhaas@gmail.com>
2023-12-21 08:41:18 -0500

    in combined long/short mode, retain any fusion calls with illumina reads that meet the min read requirements regardless of dominant isoform fraction results

--- util/filter_low_pct_dom_iso.py
+++ util/filter_low_pct_dom_iso.py
@@ -55,13 +55,22 @@ def main():
 
     retained_fusions = retained_fusions.sort_values(by=['num_LR'], ascending=False)
 
-    
+    # retain any with Illumina read support
+    if "FFPM" in filtered_out_fusions.columns.tolist():
+        recovered_FI_fusions = filtered_out_fusions[filtered_out_fusions["FFPM"] > 0]
+        num_recovered_fusions = recovered_FI_fusions.shape[0]
+        logger.info("-recovering Illumina supported fusions: {}".format(num_recovered_fusions))
+        if num_recovered_fusions > 0:
+            retained_fusions = pd.concat([retained_fusions, recovered_FI_fusions])
+            filtered_out_fusions = filtered_out_fusions[~ (filtered_out_fusions["FFPM"] > 0) ] 
+
+            
     filtered_out_fusions.to_csv(fusions_output_filename + ".removed_below_min_frac_dom_iso", sep="\t", index=False, quoting=csv.QUOTE_NONE) 
     retained_fusions.to_csv(fusions_output_filename, sep="\t", index=False, quoting=csv.QUOTE_NONE)
 
     logger.info("-filter_low_pct_dom_iso.py removed low dom iso frac fusions: " + str(filtered_out_fusions.shape[0]))
     logger.info("-filter_low_pct_dom_iso.py RETAINED above min dom iso frac fusions: " + str(retained_fusions.shape[0]))
-
+    
     
     sys.exit(0)
 
