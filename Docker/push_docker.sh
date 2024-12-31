a8c3ee2192c4ae6138edd60e97c8b1ac58ed0b10
Brian Haas <bhaas@broadinstitute.org>
2024-12-31 15:34:13 +0000

    docker update for v1.0.1

--- Docker/push_docker.sh
+++ Docker/push_docker.sh
@@ -7,3 +7,5 @@ VERSION=`cat VERSION.txt`
 docker push trinityctat/ctat_lr_fusion:${VERSION}
 docker push trinityctat/ctat_lr_fusion:latest
 
+docker run --rm -it trinityctat/ctat_lr_fusion:latest ctat-LR-fusion --version
+
