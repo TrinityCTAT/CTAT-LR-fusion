5b4afd00ae6e9400969a42fbbe818bdf4312b93e
Brian Haas <bhaas@broadinstitute.org>
2025-01-11 16:12:38 +0000

    updated docker for  v1.0.1-postdev2

--- Docker/push_docker.versioned_only.sh
+++ Docker/push_docker.versioned_only.sh
@@ -7,3 +7,4 @@ VERSION=`cat VERSION.txt`
 docker push trinityctat/ctat_lr_fusion:${VERSION}
 #docker push trinityctat/ctat_lr_fusion:latest
 
+docker run --rm -it trinityctat/ctat_lr_fusion:${VERSION} ctat-LR-fusion --version
